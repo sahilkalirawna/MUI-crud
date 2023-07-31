1a4d73b91eac1c378c8cefe4d2959a590deae0f9
Sahil kalirawna <sahilkalirawna@gmail.com>
2023-07-31 09:28:25 +0530

    API integration

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -8,7 +8,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 
 import Axios from "../../services/api/Config";
 import Table from "../../components/common/Table";
-import { deletePatientURL } from "../../services/api/routes/common";
+import { deletePatientURL, patientListURL } from "../../services/api/routes/common";
 
 import DeletePatientModel from "./components/DeletePatientModel";
 import AddEditPatientModel from "./components/AddEditPatientModel";
@@ -16,118 +16,12 @@ import AddEditPatientModel from "./components/AddEditPatientModel";
 const Dashboard = () => {
 
 	const [isDeleteLoading, setIsDeleteLoading] = useState(false);
+	const [refreshToggle, setRefreshToggle] = useState(false)
 	const [deleteOpen, setDeleteOpen] = useState(null)
 
 	const [isEdit, setIsEdit] = useState(false)
 	const [rowValue, setRowValue] = useState(null)
 	const [addEditModel, setAddEditModel] = useState(false)
-
-	const tableData = [{
-		"patientCode": "PC062",
-		"name": "Bowie Oliva",
-		"sex": "Male",
-		"category": "Non-Dependent",
-		"address": "30 Main Parkway",
-		"employeeNo": "EMP7259",
-		"_id": "dA9AcBA5DD1A60eBA4bE13B1",
-		"eligibilityGrade": "C3",
-		"bloodGroup": "B+",
-		"dateOfBirth": "1989-02-09",
-		"contactDetails": "2819706460",
-		"highRiskPatient": true,
-		"status": "Active",
-		"allergyDetails": ["64c2080dff5cddef6fca21d8"]
-	}, {
-		"patientCode": "PC932",
-		"name": "Eduardo Ilyin",
-		"sex": "Male",
-		"category": "Emergency Patient",
-		"address": "63 Forest Drive",
-		"employeeNo": "EMP4975",
-		"_id": "eaBCBc2c2f1dA7DDDF945f1A",
-		"eligibilityGrade": "A2",
-		"bloodGroup": "A+",
-		"dateOfBirth": "1995-11-13",
-		"contactDetails": "8424823045",
-		"highRiskPatient": true,
-		"status": "Terminated",
-		"allergyDetails": ["64c2080dff5cddef6fca21d8"]
-	}, {
-		"patientCode": "PC081",
-		"name": "Anthe Cloake",
-		"sex": "Female",
-		"category": "Non-Dependent",
-		"address": "892 Hagan Circle",
-		"employeeNo": "EMP4676",
-		"_id": "6dB1c8CE81ABbE2d7eE5a4A6",
-		"eligibilityGrade": "A1",
-		"bloodGroup": "A-",
-		"dateOfBirth": "1967-11-04",
-		"contactDetails": "7313524165",
-		"highRiskPatient": false,
-		"status": "Superannuated",
-		"allergyDetails": ["64c2080dff5cddef6fca21d8"]
-	}, {
-		"patientCode": "PC872",
-		"name": "Loleta Libbis",
-		"sex": "Female",
-		"category": "Non-Dependent",
-		"address": "36010 Dovetail Trail",
-		"employeeNo": "EMP8673",
-		"_id": "bEb4dB4bcdBA9B3bfD0C5b9B",
-		"eligibilityGrade": "A2",
-		"bloodGroup": "B+",
-		"dateOfBirth": "1971-03-26",
-		"contactDetails": "9454504335",
-		"highRiskPatient": false,
-		"status": "Terminated",
-		"allergyDetails": ["64c2080dff5cddef6fca21d8"]
-	}, {
-		"patientCode": "PC866",
-		"name": "Franklin Pearl",
-		"sex": "Male",
-		"category": "Emergency Patient",
-		"address": "57 Sachtjen Avenue",
-		"employeeNo": "EMP8644",
-		"_id": "BaE01c5Cad686b38bDb1498A",
-		"eligibilityGrade": "C2",
-		"bloodGroup": "AB-",
-		"dateOfBirth": "1958-01-11",
-		"contactDetails": "6149456004",
-		"highRiskPatient": false,
-		"status": "Superannuated",
-		"allergyDetails": ["64c2080dff5cddef6fca21d8"]
-	}, {
-		"patientCode": "PC408",
-		"name": "Towney Whapple",
-		"sex": "Male",
-		"category": "Employee",
-		"address": "560 West Street",
-		"employeeNo": "EMP4119",
-		"_id": "92fDfDd5AeC9AEABCdA7c7fa",
-		"eligibilityGrade": "A1",
-		"bloodGroup": "B-",
-		"dateOfBirth": "1972-11-08",
-		"contactDetails": "6763438541",
-		"highRiskPatient": false,
-		"status": "Superannuated",
-		"allergyDetails": ["64c2080dff5cddef6fca21d8"]
-	}, {
-		"patientCode": "PC377",
-		"name": "Quint Mitchelson",
-		"sex": "Male",
-		"category": "Dependent",
-		"address": "56 Hudson Crossing",
-		"employeeNo": "EMP7208",
-		"_id": "DBbaEc79BE2BaCbe1b0bfd8A",
-		"eligibilityGrade": "A3",
-		"bloodGroup": "A+",
-		"dateOfBirth": "1976-10-23",
-		"contactDetails": "2369401286",
-		"highRiskPatient": false,
-		"status": "Terminated",
-		"allergyDetails": ["64c2080dff5cddef6fca21d8"]
-	}];
 
 	const tableColumn = [
 		{
@@ -240,6 +134,7 @@ const Dashboard = () => {
 			setIsEdit(false)
 			setRowValue(null)
 		}
+		setRefreshToggle((prev) => !prev);
 	}
 
 	const handleDeleteClose = () => setDeleteOpen(null);
@@ -250,6 +145,7 @@ const Dashboard = () => {
 		Axios({ ...deletePatientURL(deleteOpen?._id) })
 			.then(() => {
 				handleDeleteClose();
+				setRefreshToggle((prev) => !prev);
 			})
 			.finally(() => {
 				setIsDeleteLoading(false);
@@ -274,8 +170,8 @@ const Dashboard = () => {
 						</div>
 						<Table
 							columns={tableColumn}
-							// dataURL={patientListURL} 
-							tableDATA={tableData}
+							dataURL={patientListURL} 
+							checkEqual={refreshToggle}
 						/>
 					</Paper>
 				</Grid>
--- src/pages/Dashboard/components/AddEditPatientModel.jsx
+++ src/pages/Dashboard/components/AddEditPatientModel.jsx
@@ -5,12 +5,12 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
 
 import {
-  Button,
-  CircularProgress,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
+	Button,
+	CircularProgress,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle,
 } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 
@@ -21,304 +21,302 @@ import HFTextInput from "../../../components/formControl/HFTextInput";
 import HFDateSelector from "../../../components/formControl/HFDateSelector";
 
 import {
-  addPatientURL,
-  // getPatientURL,
-  updatePatientURL,
+	addPatientURL,
+	getPatientURL,
+	updatePatientURL,
 } from "../../../services/api/routes/common";
 import { getDataFromObject } from "./../../../utils/common";
 import moment from "moment";
 
 const userAddEditSchema = Yup.object().shape({
-  name: Yup.string().trim().required("Name is required"),
-  sex: Yup.string().required("Gender is required"),
-  category: Yup.string().required("Category is required"),
-  address: Yup.string().trim().required("Address is required"),
-  eligibilityGrade: Yup.string().required("Eligibility Grade is required"),
-  dateOfBirth: Yup.string()
-    .required("DOB is required")
-    .typeError("Please enter a valid date")
-    .nullable()
-    .transform((date) => moment(date).format("YYYY-MM-DD")),
-  bloodGroup: Yup.string().required("Blood Group is required"),
-  contactDetails: Yup.string().required("Contact is required"),
-  status: Yup.string().required("Status is required"),
+	name: Yup.string().trim().required("Patient Name is required"),
+	sex: Yup.string().required("Gender is required"),
+	category: Yup.string().required("Category is required"),
+	address: Yup.string().trim().required("Address is required"),
+	eligibilityGrade: Yup.string().required("Eligibility Grade is required"),
+	dateOfBirth: Yup.string()
+		.required("DOB is required")
+		.typeError("Please enter a valid date")
+		.nullable()
+		.transform((date) => moment(date).format("YYYY-MM-DD")),
+	bloodGroup: Yup.string().required("Blood Group is required"),
+	contactDetails: Yup.string().required("Contact is required"),
+	status: Yup.string().required("Status is required"),
 });
 
 const AddEditPatientModel = (props) => {
-  const { isVisible = false, onClose, isEdit = false, rowValue } = props;
+	const { isVisible = false, onClose, isEdit = false, rowValue } = props;
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [isRecordSpinner, setIsRecordSpinner] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const [isRecordSpinner, setIsRecordSpinner] = useState(false);
 
-  const initialValues = {
-    name: "",
-    sex: "",
-    category: "",
-    address: "",
-    eligibilityGrade: "",
-    dateOfBirth: "",
-    bloodGroup: "",
-    contactDetails: "",
-    status: "",
-  };
+	const initialValues = {
+		name: "",
+		sex: "",
+		category: "",
+		address: "",
+		eligibilityGrade: "",
+		dateOfBirth: "",
+		bloodGroup: "",
+		contactDetails: "",
+		status: "",
+	};
 
-  const {
-    reset,
-    setValue,
-    handleSubmit,
-    control: patientControl,
-    // formState: { isValid },
-  } = useForm({
-    mode: "all",
-    reValidateMode: "onChange",
-    defaultValues: initialValues,
-    resolver: yupResolver(userAddEditSchema),
-  });
+	const {
+		reset,
+		setValue,
+		handleSubmit,
+		control: patientControl,
+		// formState: { isValid },
+	} = useForm({
+		mode: "all",
+		reValidateMode: "onChange",
+		defaultValues: initialValues,
+		resolver: yupResolver(userAddEditSchema),
+	});
 
-  useEffect(() => {
-    if (rowValue && isEdit) {
-      setIsRecordSpinner(true);
-      // Axios({ ...getPatientURL(rowValue?._id) })
-      //   .then((response) => {
-      // const res = response.data.data;
-      const res = {
-        name: "Shelia",
-        sex: "Male",
-        bloodGroup: "O+",
-        dateOfBirth: "1999/10/16",
-        category: "Emergency Patient",
-        address: "D-1204, Helios, Surat",
-        eligibilityGrade: "A1",
-        contactDetails: "+1234567891",
-        status: "Active",
-      };
-      if (res) {
-        const list = [
-          "name",
-          "sex",
-          "category",
-          "address",
-          "eligibilityGrade",
-          "dateOfBirth",
-          "bloodGroup",
-          "contactDetails",
-          "status",
-        ];
-        list.forEach((field) => {
-          setValue(field, getDataFromObject(res, field) || "");
-        });
-      }
-      // })
-      // .finally(() => {
-      setIsRecordSpinner(false);
-      // });
-    }
-  }, [isEdit, rowValue, setValue]);
+	useEffect(() => {
+		if (rowValue && isEdit) {
+			setIsRecordSpinner(true);
+			Axios({ ...getPatientURL(rowValue?._id) })
+				.then((response) => {
+					const res = response.data.data;
+					// const res = {
+					//   name: "Shelia",
+					//   sex: "Male",
+					//   bloodGroup: "O+",
+					//   dateOfBirth: "1999/10/16",
+					//   category: "Emergency Patient",
+					//   address: "D-1204, Helios, Surat",
+					//   eligibilityGrade: "A1",
+					//   contactDetails: "+1234567891",
+					//   status: "Active",
+					// };
+					if (res) {
+						const list = [
+							"name",
+							"sex",
+							"category",
+							"address",
+							"eligibilityGrade",
+							"dateOfBirth",
+							"bloodGroup",
+							"contactDetails",
+							"status",
+						];
+						list.forEach((field) => {
+							setValue(field, getDataFromObject(res, field) || "");
+						});
+					}
+				})
+				.finally(() => {
+					setIsRecordSpinner(false);
+				});
+		}
+	}, [isEdit, rowValue, setValue]);
 
-  const onSubmit = (data) => {
-    console.log(data);
-    // return;
-    setIsLoading(true);
-    if (isEdit) {
-      Axios({ ...updatePatientURL(rowValue?._id), data: data })
-        .then((res) => {
-          Toast.success(res.data.message);
-          reset(initialValues);
-          onClose();
-        })
-        .finally(() => {
-          setIsLoading(false);
-        });
-    } else {
-      Axios({ ...addPatientURL, data: data })
-        .then((res) => {
-          Toast.success(res.data.message);
-          reset(initialValues);
-          onClose();
-        })
-        .finally(() => {
-          setIsLoading(false);
-        });
-    }
-  };
+	const onSubmit = (data) => {
+		console.log(data);
+		// return;
+		setIsLoading(true);
+		if (isEdit) {
+			Axios({ ...updatePatientURL(rowValue?._id), data: data })
+				.then((res) => {
+					Toast.success(res.data.message);
+					reset(initialValues);
+					onClose();
+				})
+				.finally(() => {
+					setIsLoading(false);
+				});
+		} else {
+			Axios({ ...addPatientURL, data: data })
+				.then((res) => {
+					Toast.success(res.data.message);
+					reset(initialValues);
+					onClose();
+				})
+				.finally(() => {
+					setIsLoading(false);
+				});
+		}
+	};
 
-  const handleClose = () => {
-    reset(initialValues);
-    onClose();
-  };
+	const handleClose = () => {
+		reset(initialValues);
+		onClose();
+	};
 
-  return (
-    <Dialog
-      maxWidth="md"
-      fullWidth={true}
-      open={!!isVisible}
-      onClose={handleClose}
-      aria-labelledby="alert-dialog-title"
-      aria-describedby="alert-dialog-description"
-    >
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <DialogTitle id="alert-dialog-title">
-          {`${isEdit ? "Edit" : "Add"} Patient`}{" "}
-          {isEdit && ` - ${rowValue?.patientCode}`}
-        </DialogTitle>
-        {isRecordSpinner ? (
-          <div className="w-100 my-4 text-center">
-            <CircularProgress />
-          </div>
-        ) : (
-          <DialogContent>
-            <div className="row my-3">
-              <div className="col-md-12">
-                <HFTextInput
-                  id="name"
-                  name="name"
-                  label="Patient Name"
-                  control={patientControl}
-                />
-              </div>
-            </div>
-            <div className="row mb-3">
-              <div className="col-md-6">
-                <HFSelect
-                  id="sex"
-                  name="sex"
-                  label="Gender"
-                  control={patientControl}
-                  options={[
-                    { value: "Male", label: "Male" },
-                    { value: "Female", label: "Female" },
-                  ]}
-                />
-              </div>
-              <div className="col-md-6">
-                <HFSelect
-                  id="bloodGroup"
-                  name="bloodGroup"
-                  label="Blood Group"
-                  control={patientControl}
-                  options={[
-                    { value: "A+", label: "A+" },
-                    { value: "A-", label: "A-" },
-                    { value: "AB+", label: "AB+" },
-                    { value: "AB-", label: "AB-" },
-                    { value: "B+", label: "B+" },
-                    { value: "B-", label: "B-" },
-                    { value: "O+", label: "O+" },
-                    { value: "O-", label: "O-" },
-                  ]}
-                />
-              </div>
-            </div>
-            <div className="row mb-3">
-              <div className="col-md-6">
-                <HFDateSelector
-                  id="dateOfBirth"
-                  name="dateOfBirth"
-                  label="DOB"
-                  control={patientControl}
-                  disableFuture={true}
-                />
-              </div>
-              <div className="col-md-6">
-                <HFSelect
-                  id="category"
-                  name="category"
-                  label="Category"
-                  control={patientControl}
-                  options={[
-                    { value: "Employee", label: "Employee" },
-                    { value: "Non-Dependent", label: "Non-Dependent" },
-                    { value: "Dependent", label: "Dependent" },
-                    { value: "Emergency Patient", label: "Emergency Patient" },
-                  ]}
-                />
-              </div>
-            </div>
-            <div className="row mb-3">
-              <div className="col-md-12">
-                <HFTextInput
-                  id="address"
-                  name="address"
-                  label="Address"
-                  multiline
-                  control={patientControl}
-                />
-              </div>
-            </div>
-            <div className="row mb-3">
-              <div className="col-md-4">
-                <HFTextInput
-                  id="contactDetails"
-                  name="contactDetails"
-                  label="Contact"
-                  control={patientControl}
-                />
-              </div>
-              <div className="col-md-4">
-                <HFSelect
-                  id="eligibilityGrade"
-                  name="eligibilityGrade"
-                  label="Eligibility Grade"
-                  control={patientControl}
-                  options={[
-                    { value: "A1", label: "A1" },
-                    { value: "A2", label: "A2" },
-                    { value: "A3", label: "A3" },
-                    { value: "B1", label: "B1" },
-                    { value: "B2", label: "B2" },
-                    { value: "B3", label: "B3" },
-                    { value: "C1", label: "C1" },
-                    { value: "C2", label: "C2" },
-                    { value: "C3", label: "C3" },
-                  ]}
-                />
-              </div>
-              <div className="col-md-4">
-                <HFSelect
-                  id="status"
-                  name="status"
-                  label="Status"
-                  control={patientControl}
-                  options={[
-                    { value: "Active", label: "Active" },
-                    { value: "Terminated", label: "Terminated" },
-                    { value: "Superannuated", label: "Superannuated" },
-                  ]}
-                />
-              </div>
-            </div>
-          </DialogContent>
-        )}
-        <DialogActions>
-          <Button
-            onClick={handleClose}
-            variant="outlined"
-            sx={{ textTransform: "none" }}
-          >
-            Cancel
-          </Button>
-          <LoadingButton
-            type="submit"
-            color="primary"
-            variant="contained"
-            loadingPosition="center"
-            sx={{ textTransform: "none" }}
-            loading={isLoading}
-            // disabled={!isValid}
-          >
-            {`${isEdit ? "Update" : "Add"} Patient`}
-          </LoadingButton>
-        </DialogActions>
-      </form>
-    </Dialog>
-  );
+	return (
+		<Dialog
+			maxWidth='md'
+			fullWidth={true}
+			open={!!isVisible}
+			onClose={handleClose}
+			aria-labelledby='alert-dialog-title'
+			aria-describedby='alert-dialog-description'>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<DialogTitle id='alert-dialog-title'>
+					{`${isEdit ? "Edit" : "Add"} Patient`}{" "}
+					{isEdit && ` - ${rowValue?.patientCode}`}
+				</DialogTitle>
+				{isRecordSpinner ? (
+					<div className='w-100 my-4 text-center'>
+						<CircularProgress />
+					</div>
+				) : (
+					<DialogContent>
+						<div className='row my-3'>
+							<div className='col-md-12'>
+								<HFTextInput
+									id='name'
+									name='name'
+									label='Patient Name'
+									control={patientControl}
+								/>
+							</div>
+						</div>
+						<div className='row mb-3'>
+							<div className='col-md-6'>
+								<HFSelect
+									id='sex'
+									name='sex'
+									label='Gender'
+									control={patientControl}
+									options={[
+										{ value: "Male", label: "Male" },
+										{ value: "Female", label: "Female" },
+									]}
+								/>
+							</div>
+							<div className='col-md-6'>
+								<HFSelect
+									id='bloodGroup'
+									name='bloodGroup'
+									label='Blood Group'
+									control={patientControl}
+									options={[
+										{ value: "A+", label: "A+" },
+										{ value: "A-", label: "A-" },
+										{ value: "AB+", label: "AB+" },
+										{ value: "AB-", label: "AB-" },
+										{ value: "B+", label: "B+" },
+										{ value: "B-", label: "B-" },
+										{ value: "O+", label: "O+" },
+										{ value: "O-", label: "O-" },
+									]}
+								/>
+							</div>
+						</div>
+						<div className='row mb-3'>
+							<div className='col-md-6'>
+								<HFDateSelector
+									id='dateOfBirth'
+									name='dateOfBirth'
+									label='DOB'
+									control={patientControl}
+									disableFuture={true}
+								/>
+							</div>
+							<div className='col-md-6'>
+								<HFSelect
+									id='category'
+									name='category'
+									label='Category'
+									control={patientControl}
+									options={[
+										{ value: "Employee", label: "Employee" },
+										{ value: "Non-Dependent", label: "Non-Dependent" },
+										{ value: "Dependent", label: "Dependent" },
+										{ value: "Emergency Patient", label: "Emergency Patient" },
+									]}
+								/>
+							</div>
+						</div>
+						<div className='row mb-3'>
+							<div className='col-md-12'>
+								<HFTextInput
+									id='address'
+									name='address'
+									label='Address'
+									multiline
+									control={patientControl}
+								/>
+							</div>
+						</div>
+						<div className='row mb-3'>
+							<div className='col-md-4'>
+								<HFTextInput
+									id='contactDetails'
+									name='contactDetails'
+									label='Contact'
+									control={patientControl}
+								/>
+							</div>
+							<div className='col-md-4'>
+								<HFSelect
+									id='eligibilityGrade'
+									name='eligibilityGrade'
+									label='Eligibility Grade'
+									control={patientControl}
+									options={[
+										{ value: "A1", label: "A1" },
+										{ value: "A2", label: "A2" },
+										{ value: "A3", label: "A3" },
+										{ value: "B1", label: "B1" },
+										{ value: "B2", label: "B2" },
+										{ value: "B3", label: "B3" },
+										{ value: "C1", label: "C1" },
+										{ value: "C2", label: "C2" },
+										{ value: "C3", label: "C3" },
+									]}
+								/>
+							</div>
+							<div className='col-md-4'>
+								<HFSelect
+									id='status'
+									name='status'
+									label='Status'
+									control={patientControl}
+									options={[
+										{ value: "Active", label: "Active" },
+										{ value: "Terminated", label: "Terminated" },
+										{ value: "Superannuated", label: "Superannuated" },
+									]}
+								/>
+							</div>
+						</div>
+					</DialogContent>
+				)}
+				<DialogActions>
+					<Button
+						onClick={handleClose}
+						variant='outlined'
+						sx={{ textTransform: "none" }}>
+						Cancel
+					</Button>
+					<LoadingButton
+						type='submit'
+						color='primary'
+						variant='contained'
+						loadingPosition='center'
+						sx={{ textTransform: "none" }}
+						loading={isLoading}
+						// disabled={!isValid}
+					>
+						{`${isEdit ? "Update" : "Add"} Patient`}
+					</LoadingButton>
+				</DialogActions>
+			</form>
+		</Dialog>
+	);
 };
 
 AddEditPatientModel.propTypes = {
-  isEdit: PropTypes.bool,
-  isVisible: PropTypes.bool,
-  onClose: PropTypes.func,
-  rowValue: PropTypes.any,
+	isEdit: PropTypes.bool,
+	isVisible: PropTypes.bool,
+	onClose: PropTypes.func,
+	rowValue: PropTypes.any,
 };
 
 export default AddEditPatientModel;
